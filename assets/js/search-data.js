41d9402f9ef1086f53fed0935de6521863cebc1e
xmainguyen <maixuannguyen96@gmail.com>
2025-09-19 06:25:22 +0000

    Deploying to gh-pages from @ xmainguyen/xmainguyen.github.io@bf2edd4e3739807ad63f56c2b6691bf9bc77a40f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,35 +466,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%69%78%6E%6E%67%75%79%65%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/xmainguyen", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/xmainguyen", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-strava',
+        title: 'Strava',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.strava.com/athletes/tdovStOzMWb", "_blank");
         },
       },{
       id: 'light-theme',
