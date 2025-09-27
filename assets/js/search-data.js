84e20aee757d6742b9cb2a5c4660906ba4b80ad6
xmainguyen <maixnnguyen@gmail.com>
2025-09-27 21:58:17 +0000

    Deploying to gh-pages from @ xmainguyen/xmainguyen.github.io@de1717e76336660917527f78d2713f11f1fa697a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
