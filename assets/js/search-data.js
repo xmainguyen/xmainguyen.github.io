e572bfc2812a11a5ab020882eed65c3ae2c49412
xmainguyen <maixnnguyen@gmail.com>
2025-10-22 03:30:16 +0000

    Deploying to gh-pages from @ xmainguyen/xmainguyen.github.io@5b9adfecbf7019fbfefbfe89b5dc280b3ff8fb26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/media/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-repositories",
           title: "Repositories",
           description: "",
